551b4dfeb81d019422612b56a4a3079b43f1a37b
charlespaul1 <charlespaulwabomba@gmail.com>
2022-09-20 03:21:53 -0700

    alphabet in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-#using ascii chart range for small letters
 for i in range(97, 123):
-    #changing the ascii representation to characters and printing
-    print(chr(i), end="")
+    print("{:c}".format(i),end="")
