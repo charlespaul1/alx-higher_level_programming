4307a0e5ca8b75a86aff015a6f244eb3c417cd60
charlespaul1 <charlespaulwabomba@gmail.com>
2022-09-25 00:15:14 -0700

    adding tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+#add the index 0 of a and b together, index 1 of a and b together
+    return (tuple_a [0] + tuple_b[0], tuple_a[1] + tuple_b[1])
