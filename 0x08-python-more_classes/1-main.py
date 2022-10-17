0810fe21297bbc7487cf76264337bec8fff118b4
charlespaul1 <charlespaulwabomba@gmail.com>
2022-10-17 07:45:40 -0700

    area and perimeter of the rectangle

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 Rectangle = __import__('1-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
+my_rectangle = Rectangle('w', 4)
 print(my_rectangle.__dict__)
 
-my_rectangle.width = 10
-my_rectangle.height = 3
+my_rectangle.width = '-1'
+my_rectangle.height = 0.9
 print(my_rectangle.__dict__)
 
