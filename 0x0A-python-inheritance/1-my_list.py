44e830279ac1f292d3ee85a7f8347b8fc3d18811
charlespaul1 <charlespaulwabomba@gmail.com>
2022-10-24 03:26:46 -0700

    tests

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,8 +2,6 @@
 """simple my list class"""
 
 class MyList(list):
-    """simple class MyList, it inherits from list"""
-    def print_sorted(self):
-        """prints a sorted list"""
+    """return a sorted list"""
 
-        return(sorted(self))
+    return(sorted(self))
