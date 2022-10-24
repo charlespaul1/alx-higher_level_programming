5e97423b129082f92576102c601c115907f4ca88
charlespaul1 <charlespaulwabomba@gmail.com>
2022-10-24 06:50:10 -0700

    pycodestytyle fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,12 +4,12 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-        """Rectangle class, inherits from BaseGeometry"""
+    """Rectangle class, inherits from BaseGeometry"""
 
-        def __init__(self, width, height):
-            """Constructor for Rectangle class"""
+    def __init__(self, width, height):
+        """Constructor for Rectangle class"""
 
-            super().integer_validator("width", width)
-            super().integer_validator("height", height)
-            self.__width = width
-            self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
