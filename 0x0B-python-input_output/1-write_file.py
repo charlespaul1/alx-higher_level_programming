ed99b266277a2e7f760fb63728b79d834b79fb43
charlespaul1 <charlespaulwabomba@gmail.com>
2022-10-25 10:50:13 -0700

    chan

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+""" function that writes a string to a text file (UTF8)"""
+
+
 def write_file(filename="", text=""):
     with open(filename, mode="w") as fd:
         nbc = fd.write(text)
