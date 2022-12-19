96110b6ef23ba29dce6b34bb8c1ff09c2a90bf7e
charlespaul1 <charlespaulwabomba@gmail.com>
2022-12-19 17:17:32 +0300

    a script that prints 3 lines, by using an array and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 let myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0, len = myArray.length; i < len; i++) {
-  console.log(myArray[i]);
+	console.log(myArray[i]);
 }
