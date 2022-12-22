1515db94d0f68341277c2017b4bc593c1353f931
charlespaul1 <charlespaulwabomba@gmail.com>
2022-12-22 16:20:19 +0300

    class def of a city &  a script that prints all City objects from the database hbtn_0e_14_usa:

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this modules contains one class
+that represent the Cities model
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    represent  cities table
+    """
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
